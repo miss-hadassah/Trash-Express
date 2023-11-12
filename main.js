b0afb6125dfb253db93afb3ba11e35159443560f
Nafo Legior Joseph <estherjosephnafo@gmail.com>
2023-11-12 02:16:36 +0100

    bug

--- main.js
+++ main.js
@@ -1,45 +1,23 @@
-const contactForm = document.querySelector("#form");
-const submitBtn = document.querySelector(".button");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const numberInput = document.querySelector("#number");
-const addressInput = document.querySelector("#address");
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_8p4r7ln';
+   const templateID = 'template_vsj7lsj';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
 
 
 
-
-const publicKey = "E93ocs3x1ZqY5lA1O";
-const serviceID = "service_8p4r7ln";
-const templateID ="template_vsj7lsj";
-
-
-emailjs.init("publicKey");
-    
-contactForm.addEventListener("button", e => {
-  e.preventDefault();
-
-  button.innerText = "Just A Moment...";
-
-  const field = {
-    name: nameInput.value,
-    email: emailInput.value,
-    number: numberInput.value,
-    address: addressInput.value,
-  }
-
-  emailjs.send(serviceID, templateID, field)
-  .then(() => {
-    button.innerText = "sent successfully";
-    nameInput.value = "";
-    emailInput.value = "";
-    numberInput.value = "";
-    addressInput.value = "";
-  }, (error) => {
-
-    console.log(error);
-    button.innerText = "Something went wrong";
-  });
-
- });
-  
-   
